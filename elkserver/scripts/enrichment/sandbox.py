96de12165679ab4f57db3d03e7abe99667aca71a
ATM Red Team Operator <rt-op@rto-01.rt.atmo.cgi.com>
2019-10-02 09:45:26 -0400

    Adjust the query for sandboxes.

--- elkserver/scripts/enrichment/sandbox.py
+++ elkserver/scripts/enrichment/sandbox.py
@@ -8,7 +8,7 @@ class KnownSandboxEnrichment(EnrichmentPlugin):
     def run(self):        
         new_sandboxes = []
         for index in ["beacondb", "rtops-*"]:
-            for suspect_beacon in self.run_query(index, "target_hostname.keyword :*-PC"):
+            for suspect_beacon in self.run_query(index, "NOT (tags:sandboxes_v01 OR tags:testsystems_v01) AND (target_hostname.keyword :*-PC)"):
                 user = re.sub(' \*', '', suspect_beacon["_source"]["target_user"]).lower()
 
                 if suspect_beacon["_source"]["target_hostname"].lower().startswith(user):
@@ -19,9 +19,17 @@ class KnownSandboxEnrichment(EnrichmentPlugin):
                     new_sandboxes.append(suspect_beacon["_source"]["target_hostname"])
 
             for state in self.state:
+                if state.startswith("#"):
+                    continue
+
                 state_fields = state.split(";")
-                keyword = state_fields.pop()
-                for suspect_beacon in self.run_query(index, f" {keyword} ".join(state_fields)):                  
+
+                if len(state_fields) != 4:
+                    print(f"Missing  or malformed state line: {state}")
+                    continue
+                keyword = state_fields.pop(0)
+                query = f"NOT (tags:sandboxes_v01 OR tags:testsystems_v01) AND (target_user:{state_fields[0]} {keyword} target_hostname.keyword:{state_fields[1]} {keyword} target_ipint:{state_fields[2]})"
+                for suspect_beacon in self.run_query(index, query):                  
                     suspect_beacon["_source"]["tags"].extend(["sandboxes_v01", "sandbox_calculated"])
                     suspect_beacon["_source"]["tags"] = list(set(suspect_beacon["_source"]["tags"]))
                     self.update(suspect_beacon)
